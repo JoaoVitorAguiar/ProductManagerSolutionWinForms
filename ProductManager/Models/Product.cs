f2e32c81d7585f18160fba71767528ac38d88e11
VITOR AGUIAR <joaovitor4guiar@gmail.com>
2023-09-15 13:52:53 -0300

    feat: add model Product

---
+++ ProductManager/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductManager.Models;
+
+[Table("Products")]
+public class Product
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [StringLength(80)]
+    public string Name { get; set; }
+
+    [Precision(18, 2)]
+    public decimal Price { get; set; }
+}
