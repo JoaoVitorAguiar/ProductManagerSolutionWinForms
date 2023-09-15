4c3553eaf5929a9b9f2e46e047bf848e81d397fc
VITOR AGUIAR <joaovitor4guiar@gmail.com>
2023-09-15 13:52:33 -0300

    feat: add data context

---
+++ ProductManager/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductManager.Data;
+
+public class DataContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("");
+    }
+}
